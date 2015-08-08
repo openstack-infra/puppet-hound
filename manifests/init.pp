6ce0881b6ad5b36744dd24d6b81a3d932cc7c2df
Emma Barber <me@taron.io>
2015-08-07 19:43:47 -0700

    Configure and run apache on top of hound
    Creates a service user and runs hound and apache. The config.json can
    optionally be unmanaged but is otherwise taken from a file.
    
    Co-Authored-By: Monty Taylor <mordred@inaugust.com>
    Change-Id: I4042594421989ab60d1134fbfcfc8b0c73839faa

---
+++ manifests/init.pp
@@ -0,0 +1,119 @@
+# Class to configure hound
+#
+# == Class: hound
+class hound (
+  $manage_config = true,
+  $config_json = 'hound/config.json',
+  $vhost_name = $::fqdn,
+  $datadir = '/home/hound/data',
+  $serveradmin = "webmaster@${::fqdn}",
+) {
+
+  package { 'golang':
+    ensure => present,
+  }
+
+  user { 'hound':
+    ensure     => present,
+    home       => '/home/hound',
+    shell      => '/bin/bash',
+    gid        => 'hound',
+    managehome => true,
+    require    => Group['hound'],
+  }
+
+  group { 'hound':
+    ensure => present,
+  }
+
+  file {'/home/hound':
+    ensure  => directory,
+    owner   => 'hound',
+    group   => 'hound',
+    mode    => '0755',
+    require => User['hound'],
+  }
+
+  file { $datadir:
+    ensure  => 'directory',
+    owner   => 'hound',
+    group   => 'hound',
+    require => User['hound'],
+  }
+
+  if $manage_config {
+    file { '/home/hound/config.json':
+      ensure  => 'present',
+      owner   => 'hound',
+      group   => 'hound',
+      content => file($config_json),
+      require => User['hound'],
+    }
+  }
+
+  vcsrepo { '/home/hound/src/github.com/etsy/hound':
+    ensure   => latest,
+    provider => git,
+    source   => 'git://github.com/etsy/Hound.git',
+    notify   => Exec['build_hound'],
+  }
+
+  exec { 'build_hound':
+    command     => 'go install github.com/etsy/hound/cmds/...',
+    path        => '/bin:/usr/bin:/usr/local/bin',
+    environment => 'GOPATH=/home/hound',
+    cwd         => '/home/hound',
+    creates     => '/home/hound/bin/houndd',
+    user        => 'hound',
+    timeout     => 600,
+    require     => [
+      User['hound'],
+      Package['golang'],
+    ]
+  }
+
+  service { 'hound':
+    ensure     => running,
+    hasrestart => true,
+    require    => [
+      File['/etc/init.d/hound'],
+      Exec['build_hound'],
+    ],
+    subscribe  => File['/home/hound/config.json'],
+  }
+
+  file { '/etc/init.d/hound':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/hound/hound.init',
+    notify => Service['hound'],
+    before => Service['hound'],
+  }
+
+  include ::httpd
+
+  httpd_mod { 'rewrite':
+    ensure => present,
+    before => Service['hound'],
+  }
+
+  httpd_mod { 'proxy':
+    ensure => present,
+    before => Service['hound'],
+  }
+
+  httpd_mod { 'proxy_http':
+    ensure => present,
+    before => Service['hound'],
+  }
+
+  httpd::vhost { $vhost_name:
+    port     => 80,
+    docroot  => 'MEANINGLESS ARGUMENT',
+    priority => '50',
+    template => 'hound/hound.vhost.erb',
+  }
+
+}
